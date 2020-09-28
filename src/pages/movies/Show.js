18183e485c5dc02482f842043ec3ef32aa04a7ba
ridhoajibx <ridhoajibx@gmail.com>
2020-09-28 20:36:20 +0700

    [ridho]--refactor svg

--- src/pages/movies/Show.js
+++ src/pages/movies/Show.js
@@ -22,8 +22,8 @@ const Show = () => {
         <div className="container py-4">
             <div className="d-flex justify-content-end align-items-center mt-4">
                 <Link to="/" className="btn btn-primary btn-small d-flex align-items-center">
-                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"></path>
+                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"></path>
                     </svg>
                     Back
                 </Link>
